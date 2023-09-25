62f5e7dbf1f0388b455fdb8dc2211343e4d6a390
Tassadit Idiri <mamantasse@gmail.com>
2023-09-25 20:11:43 +0200

    css

--- Front-End-Fisheye-main/scripts/index.js
+++ Front-End-Fisheye-main/scripts/index.js
@@ -1,38 +1,37 @@
-// Récupérez la référence de l'élément HTML où vous souhaitez afficher la liste des photographes
+// je récupére la référence de l'élément HTML où vous souhaitez afficher la liste des photographes
 const photographerSection = document.querySelector(".photographer_section");
 
-// Utilisez fetch() pour charger le fichier JSON local
+function getUserCardDOM() {
+  const article = document.createElement("article");
+  const img = document.createElement("img");
+  img.setAttribute("src", picture);
+  const h2 = document.createElement("h2");
+  h2.textContent = name;
+  article.appendChild(img);
+  article.appendChild(h2);
+  return article;
+}
+
+// j'utilise fetch() pour charger le fichier JSON local
 function photographerTemplate(data) {
   const { name, portrait } = data;
-
   const picture = `assets/photographers/${portrait}`;
-
-  function getUserCardDOM() {
-    const article = document.createElement("article");
-    const img = document.createElement("img");
-    img.setAttribute("src", picture);
-    const h2 = document.createElement("h2");
-    h2.textContent = name;
-    article.appendChild(img);
-    article.appendChild(h2);
-    return article;
-  }
-  return { name, picture, getUserCardDOM };
+return { name, picture, getUserCardDOM };
 }
 
 fetch("./data/photographers.json")
   .then((response) => response.json())
   .then((data) => {
-    // Maintenant, vous avez accès aux données JSON dans la variable "data"
+    // Maintenant, on a acces aux données JSON dans la variable "data"
 
-    // Assurez-vous que le fichier JSON a une structure avec un tableau "photographers"
+  
     if (data.photographers) {
-      // Parcourez le tableau des photographes et créez un élément HTML pour chacun d'eux
+      // on parcoure le tableau des photographes et crée un élément HTML pour chacun d'eux
       data.photographers.forEach((photographer) => {
         const photographerElement = document.createElement("article");
         photographerElement.classList.add("photographer");
 
-        // Créez le contenu HTML pour chaque photographe (par exemple, le nom et la ville)
+        // je crée le contenu HTML pour chaque photographe 
         photographerElement.innerHTML = `
           <img src="assets/photographers/Sample Photos/Photographers ID Photos/${photographer.portrait}" alt="${photographer.name}" class="portrait">
           <h2>${photographer.name}</h2>
@@ -41,7 +40,7 @@ fetch("./data/photographers.json")
           <p class="info">Price: ${photographer.price}</p>
         `;
 
-        // Ajoutez l'élément du photographe à la section des photographes
+        // j'ajoute l'élément du photographe à la section des photographes
         photographerSection.appendChild(photographerElement);
       });
     }
