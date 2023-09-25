a6b7fef17aa7dd89b92737cccada8bf2dc208aa1
Tassadit Idiri <mamantasse@gmail.com>
2023-09-25 19:38:41 +0200

    page d'accueil recup photographe

--- Front-End-Fisheye-main/scripts/pages/index.js
+++ Front-End-Fisheye-main/scripts/pages/index.js
@@ -1,46 +1,54 @@
-    async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    }
+// Récupérez la référence de l'élément HTML où vous souhaitez afficher la liste des photographes
+const photographerSection = document.querySelector(".photographer_section");
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+// Utilisez fetch() pour charger le fichier JSON local
+function photographerTemplate(data) {
+  const { name, portrait } = data;
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerTemplate(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    }
+  const picture = `assets/photographers/${portrait}`;
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    article.appendChild(img);
+    article.appendChild(h2);
+    return article;
+  }
+  return { name, picture, getUserCardDOM };
+}
+
+fetch("./data/photographers.json")
+  .then((response) => response.json())
+  .then((data) => {
+    // Maintenant, vous avez accès aux données JSON dans la variable "data"
+
+    // Assurez-vous que le fichier JSON a une structure avec un tableau "photographers"
+    if (data.photographers) {
+      // Parcourez le tableau des photographes et créez un élément HTML pour chacun d'eux
+      data.photographers.forEach((photographer) => {
+        const photographerElement = document.createElement("article");
+        photographerElement.classList.add("photographer");
+
+        // Créez le contenu HTML pour chaque photographe (par exemple, le nom et la ville)
+        photographerElement.innerHTML = `
+          <img src="assets/photographers/Sample Photos/Photographers ID Photos/${photographer.portrait}" alt="${photographer.name}" class="portrait">
+          <h2>${photographer.name}</h2>
+          <p class="infoVille">${photographer.city}, ${photographer.country}</p>
+          <p class="info">${photographer.tagline}</p>
+          <p class="info">Price: ${photographer.price}</p>
+        `;
+
+        // Ajoutez l'élément du photographe à la section des photographes
+        photographerSection.appendChild(photographerElement);
+      });
     }
-    
-    init();
-    
+  })
+  .catch((error) => {
+    console.error(
+      "Une erreur s'est produite lors de la récupération des données JSON :",
+      error
+    );
+  });
