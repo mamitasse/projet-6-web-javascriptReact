ebc97d1278f4dbed20e52f0008d7e08c692692d8
mamitasse <117809555+mamitasse@users.noreply.github.com>
2023-10-16 09:34:01 +0200

    retouche css et debut navigation avec fleche

--- Front-End-Fisheye-main/scripts/views/DetailPhotographerView.js
+++ Front-End-Fisheye-main/scripts/views/DetailPhotographerView.js
@@ -1,15 +1,24 @@
+// Cette classe gère l'affichage des détails du photographe et la galerie de médias
 class DetailPhotographerView {
   constructor() {
+    // Initialisation des variables et éléments HTML
     this.totalMedia = [];
     this.currentMediaIndex = 0;
     this.lightbox = document.getElementById("lightbox");
     this.lightboxMedia = document.querySelector(".lightbox__media");
     this.lightboxTitle = document.querySelector(".lightbox__title");
 
-    // Sélectionnez les boutons Next et Previous et ajoutez des écouteurs d'événements
+    // Sélectionnez les boutons Next, Previous et Close pour ajouter des écouteurs d'événements
     this.nextButton = document.querySelector(".lightbox__next");
     this.previousButton = document.querySelector(".lightbox__previous");
+    this.closeButton = document.querySelector(".close-btn");
 
+    // Gestion de la touche "Escape" pour fermer la lightbox
+    document.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape') this.closeLightbox();
+    });
+
+    // Écouteurs d'événements pour les boutons Next et Previous
     this.nextButton.addEventListener("click", () => {
       this.showNextMedia();
     });
@@ -18,13 +27,22 @@ class DetailPhotographerView {
       this.showPreviousMedia();
     });
 
+    // Écouteurs d'événements pour la navigation à l'aide des touches de clavier (flèches gauche/droite)
+    document.addEventListener('keydown', (e) => {
+      if (e.key === 'ArrowLeft') this.showPreviousMedia();
+      if (e.key === 'ArrowRight') this.showNextMedia();
+    });
+
+    // Appel à la fonction pour configurer les gestionnaires d'événements des médias
     this.setupMediaEventHandlers();
   }
 
+  // Cette méthode affiche les détails du photographe et sa galerie de médias
   displayPhotographerDetails(photographer, totalMedia) {
     this.totalMedia = totalMedia;
     this.photographer = photographer;
 
+    // Affichage des informations du photographe
     const photographerHeader = document.querySelector(".photograph-header");
     photographerHeader.innerHTML = `
       <div class="photographer-info">
@@ -36,13 +54,13 @@ class DetailPhotographerView {
       <div class="photographer-photo">
         <img src="assets/photographers/Sample Photos/Photographers ID Photos/${photographer.portrait}" alt="${photographer.name}" />
       </div>
-      
     `;
-    const titreModal=document.querySelector(".titreModal");
-    titreModal.innerHTML=`Contactez-moi <br>${photographer.name}`;
-    console.log(photographer.name);
 
+    // Mise à jour du titre du modal de contact
+    const titreModal = document.querySelector(".titreModal");
+    titreModal.innerHTML = `Contactez-moi <br>${photographer.name}`;
 
+    // Affichage des médias dans la galerie
     const mediaContainer = document.getElementById("catalogue");
     mediaContainer.innerHTML = "";
 
@@ -62,6 +80,7 @@ class DetailPhotographerView {
           </div>
         `;
 
+        // Ajout d'un gestionnaire d'événements pour ouvrir la lightbox lors du clic
         mediaElement.addEventListener("click", () => {
           this.openLightbox(index, photographer);
         });
@@ -74,6 +93,7 @@ class DetailPhotographerView {
           </div>
         `;
 
+        // Ajout d'un gestionnaire d'événements pour ouvrir la lightbox lors du clic
         mediaElement.addEventListener("click", () => {
           this.openLightbox(index, photographer);
         });
@@ -83,6 +103,7 @@ class DetailPhotographerView {
     });
   }
 
+  // Cette méthode trie les médias par popularité (likes)
   sortMediaByPopularity() {
     const mediaContainer = document.getElementById("catalogue");
     const mediaElements = Array.from(mediaContainer.querySelectorAll(".media"));
@@ -98,6 +119,7 @@ class DetailPhotographerView {
     });
   }
 
+  // Cette méthode trie les médias par date
   sortMediaByDate() {
     const mediaContainer = document.getElementById("catalogue");
     const mediaElements = Array.from(mediaContainer.querySelectorAll(".media"));
@@ -113,6 +135,7 @@ class DetailPhotographerView {
     });
   }
 
+  // Cette méthode trie les médias par titre
   sortMediaByTitle() {
     const mediaContainer = document.getElementById("catalogue");
     const mediaElements = Array.from(mediaContainer.querySelectorAll(".media"));
@@ -126,20 +149,29 @@ class DetailPhotographerView {
     mediaElements.forEach((mediaElement) => {
       mediaContainer.appendChild(mediaElement);
     });
-  
   }
 
-
+  // Configuration des gestionnaires d'événements pour les médias
   setupMediaEventHandlers() {
     const mediaElements = document.querySelectorAll("#catalogue .media img, #catalogue .media video");
 
     mediaElements.forEach((media, index) => {
-      media.addEventListener("click", () => {
-        this.openLightbox(index, this.photographer); // Utilisez this.photographer
+      media.addEventListener("click", (e) => {
+        // Empêche l'ouverture de la lightbox au clic
+        e.stopPropagation();
+        e.preventDefault();
+        
+        // Affiche la lightbox uniquement si la touche "Entrée" est pressée
+        media.addEventListener("keydown", (e) => {
+          if (e.key === 'Enter') {
+            this.openLightbox(index, this.photographer);
+          }
+        });
       });
     });
   }
 
+  // Cette méthode ouvre la lightbox avec le média sélectionné
   openLightbox(index, photographer) {
     this.currentMediaIndex = index;
     const media = this.totalMedia[index];
@@ -161,6 +193,7 @@ class DetailPhotographerView {
     this.lightbox.style.display = "block";
   }
 
+  // Cette méthode affiche le média suivant dans la lightbox
   showNextMedia() {
     if (this.currentMediaIndex < this.totalMedia.length - 1) {
       this.currentMediaIndex++;
@@ -168,6 +201,7 @@ class DetailPhotographerView {
     }
   }
 
+  // Cette méthode affiche le média précédent dans la lightbox
   showPreviousMedia() {
     if (this.currentMediaIndex > 0) {
       this.currentMediaIndex--;
@@ -175,15 +209,16 @@ class DetailPhotographerView {
     }
   }
 
+  // Cette méthode ferme la lightbox
   closeLightbox() {
     this.lightbox.style.display = "none";
   }
- 
 }
 
-
+// Création d'une instance de DetailPhotographerView
 const detailPhotographerView = new DetailPhotographerView();
 
+// Gestion du bouton pour afficher/masquer la liste de tri
 const toggleDropdownButton = document.getElementById("toggleDropdown");
 const dropdownContent = document.getElementById("dropdownContent");
 const chevron = document.querySelector(".chevron");
@@ -198,6 +233,7 @@ toggleDropdownButton.addEventListener("click", () => {
   }
 });
 
+// Gestion des boutons de tri
 const sortByTitle = document.getElementById("sortByTitle");
 sortByTitle.addEventListener("click", () => {
   detailPhotographerView.sortMediaByTitle();
@@ -208,7 +244,19 @@ sortByDate.addEventListener("click", () => {
   detailPhotographerView.sortMediaByDate();
 });
 
+// Gestion du bouton de fermeture de la lightbox
 const closeButton = document.querySelector(".close-btn");
 closeButton.addEventListener("click", () => {
   detailPhotographerView.closeLightbox();
+});
+
+// Autres gestionnaires d'événements (par exemple, pour la navigation à l'aide des touches de clavier)
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'ArrowLeft') {
+    detailPhotographerView.showPreviousMedia();
+
+  } else if (e.key === 'ArrowRight') {
+    detailPhotographerView.showNextMedia();
+    
+  }
 });
